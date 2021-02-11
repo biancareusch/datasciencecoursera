e467238a41ddb50ba4a9b140e356e61941f5cf70
biancareusch <reusch.bianca@gmail.com>
2021-02-11 14:13:20 -0600

    explicit coercion

--- testing.R
+++ testing.R
@@ -14,3 +14,7 @@
  d <- 9:29 ## integer vector
  e <- c(TRUE, 2) ##type coercion forces every element to be the same class(least common denominator = 2 so TRUE becomes numerical)
  
+ 
+ ##explicit coercion
+ as.numeric(x) ##turns x into numeric objects
+ as.character(x) ## turns x into characters
