faeb2df4a8a6cd5a42ee2a6d051a4c681b40ce5f
biancareusch <reusch.bianca@gmail.com>
2021-02-11 14:11:41 -0600

    creating vectors and type coercion

--- testing.R
+++ testing.R
@@ -3,3 +3,14 @@
  z <- 30
  
  print(x)
+ ## or
+ x
+
+ ##Comments
+ 
+ a <- c(0.5,0.6) ##numeric vector
+ b <- c(T,F)     ##logical vector
+ c <- c("a","b","c") ##character vector
+ d <- 9:29 ## integer vector
+ e <- c(TRUE, 2) ##type coercion forces every element to be the same class(least common denominator = 2 so TRUE becomes numerical)
+ 
